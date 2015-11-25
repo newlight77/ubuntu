e44a7464516f8bff53cabb95e738ca3ad45141a2
Kong TO <kong.to3@docapost.fr>
2015-11-25 11:41:11 +0100

    Updating scripts

---
+++ removeNode.js
@@ -0,0 +1,19 @@
+sudo npm uninstall npm -g
+sudo apt-get remove nodejs
+
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+
+sudo rm -rf ~/.npm
+sudo rm -fr ~/.node-gyp
+
+ls -laF /usr/local/{lib/node{,/.npm,_modules},bin,share/man}/{npm*,node*,man1/node*}
+
+sudo rm -fr /usr/lib/node_modules
+sudo rm -rf /usr/local/{lib/node{,/.npm,_modules},bin,share/man}/{npm*,node*,man1/node*}
+
+#sudo rm -fr /usr/local/lib/node
+#sudo rm -fr /usr/local/lib/node_modules
+#sudo rm -rf /usr/local/bin/npm
+#sudo rm -rf /usr/local/lib/dtrace/node.d
+#sudo rm -rf /usr/local/share/man/man*/node*
+#sudo rm -rf /usr/local/share/man/man*/npm*
