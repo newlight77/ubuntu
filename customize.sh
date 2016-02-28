140a8842e98bbbcf29e363842b30617929f91e29
Kong To <newlight77@gmail.com>
2016-02-28 23:55:56 +0100

    Updating scripts for sourcing bash profile

--- customize.sh
+++ customize.sh
@@ -2,13 +2,17 @@
 
 echo "*** Customize *** Customizing User env" 1>&2
 
-cd ~/ubuntu
-cp  .bashrc_profile >> ~/.bashrc_profile
-cp  .bashrc_alias >> ~/.bashrc_alias
-cat .bashrc >> ~/.bashrc
-cat .gitconfig >> ~/.gitconfig
+cd ~
+
+git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt
+git clone https://github.com/google/styleguide.git
+git clone https://github.com/newlight77/ubuntu.git .ubuntu
+
+cat .ubuntu/.bashrc_profile >> ~/.bashrc_profile
+cat .ubuntu/.gitconfig >> ~/.gitconfig
+
+
 git config --global user.name "newlight77"
 git config --global user.email "newlight77@gmail.com"
-git clone https://github.com/google/styleguide.git
 
 ssh-keygen -t rsa
