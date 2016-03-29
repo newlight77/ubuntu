cb93e90479c9da57290178a26b1602f7473ca735
Kong TO <kong.to3@docapost.fr>
2016-03-29 17:48:59 +0200

    update script

--- customize.sh
+++ customize.sh
@@ -5,18 +5,13 @@ echo "*** Customize *** Customizing User env" 1>&2
 git clone https://github.com/google/styleguide.git
 #git clone https://github.com/newlight77/ubuntu.git .ubuntu
 
-cp  .bashrc_natelandau  ~/.ubuntu/.bashrc_natelandau
+cp  .bashrc_natelandau ~/.ubuntu/.bashrc_natelandau
 cp  .bashrc_profile    ~/.ubuntu/.bashrc_profile
 cp  .bashrc_alias      ~/.ubuntu/.bashrc_alias
-cp  .gitconfig_alias      ~/.ubuntu/.gitconfig_alias
+cp  .gitconfig_alias   ~/.ubuntu/.gitconfig_alias
 
-cat .bashrc       >> ~/.bashrc
-cat .gitconfig    >> ~/.gitconfig
-
-git config --global user.name "newlight77"
-git config --global user.email "newlight77@gmail.com"
-
-git clone https://github.com/google/styleguide.git
+cat .bashrc_profile    >> ~/.bashrc
+cat .gitconfig         >> ~/.gitconfig
 
 cd ~
 git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt
