3dc9ed1c78caf81bde862ebf064b41b44ef48d24
Kong TO <kong.to3@docapost.fr>
2016-03-29 15:05:30 +0200

    update script

--- customize.sh
+++ customize.sh
@@ -5,14 +5,12 @@ echo "*** Customize *** Customizing User env" 1>&2
 git clone https://github.com/google/styleguide.git
 #git clone https://github.com/newlight77/ubuntu.git .ubuntu
 
-cp  .bashrc_natenadau  ~/.ubuntu/.bashrc_natenadau
+cp  .bashrc_natelandau  ~/.ubuntu/.bashrc_natelandau
 cp  .bashrc_profile    ~/.ubuntu/.bashrc_profile
 cp  .bashrc_alias      ~/.ubuntu/.bashrc_alias
-
 cp  .gitconfig_alias      ~/.ubuntu/.gitconfig_alias
 
 cat .bashrc       >> ~/.bashrc
-
 cat .gitconfig    >> ~/.gitconfig
 
 git config --global user.name "newlight77"
