ceb6de594b251eaf567f99836ed0e54beca3f9e6
Kong TO <newlight77@users.noreply.github.com>
2016-04-26 14:31:39 +0200

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -16,6 +16,7 @@ cp  .bashrc_profile    ~/.ubuntu/.bashrc_profile
 cp  .bashrc_alias      ~/.ubuntu/.bashrc_alias
 cp  .bashrc_ps12       ~/.ubuntu/.bashrc_ps12
 cp  .gitconfig_alias   ~/.ubuntu/.gitconfig_alias
+cp  .bashrc            ~/.ubuntu/.basrch
 
 cat .bashrc_profile    >> ~/.bashrc
 cat .gitconfig         >> ~/.gitconfig
