da6ec93177e6ccc054be6340bfffb15ec047e762
Kong To <newlight77@gmail.com>
2016-05-13 10:50:21 +0200

    Adding installation of startUML

--- installSystemTools.sh
+++ installSystemTools.sh
@@ -199,3 +199,17 @@ installUnitTweak () {
   apt-get install gpick -y >> $LOGFILE
   echo "***installUnitTweak*** done" 1>&2
 }
+
+installStarUml() {
+  echo "***installStarUml*** Installing StarUml" 1>&2
+  $(isRoot)
+  version=1.5.3
+  wget https://launchpad.net/ubuntu/+archive/primary/+files/libgcrypt11_$version-2ubuntu4.2_amd64.deb
+  dpkg -i libgcrypt11_$version-2ubuntu4.2_amd64.deb >> $LOGFILE
+  rm libgcrypt11_$version-2ubuntu4.2_amd64.deb
+  version=2.7.0
+  wget http://staruml.io/download/release/v$version/StarUML-v$version-64-bit.deb
+  dpkg -i StarUML-v$version-64-bit.deb >> $LOGFILE
+  rm StarUML-v$version-64-bit.deb
+  echo "***installStarUml*** done" 1>&2
+}
