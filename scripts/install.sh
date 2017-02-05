ed5cb728a04e4f557ecc260df2737c232e39eef6
Kong To <newlight77@gmail.com>
2017-02-05 19:29:29 +0100

    installing jdk silently

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,26 +5,28 @@ if [ ! -d "$DIR" ]; then DIR="$PWD"; fi
 
 . $DIR/isRoot.sh
 
+LOGFILE=`basename $0`.log
+
 install() {
   echo "*** ------ install ------ ***" 1>&2
   $(isRoot)
 
-  $DIR/install/installSystemPackages.sh
-  $DIR/install/installJdk.sh # interactive
+  $DIR/install/installSystemPackages.sh >> $LOGFILE
+  $DIR/install/installJdk.sh >> $LOGFILE # interactive
 
-  $DIR/install/installMailUtil.sh
-  $DIR/install/installMkUsb.sh
+  $DIR/install/installMailUtil.sh >> $LOGFILE
+  $DIR/install/installMkUsb.sh >> $LOGFILE
 
-  #$DIR/install/installMaven.sh
-  #$DIR/install/installTomcat8.sh
-  $DIR/install/installNodejs.sh
-  $DIR/install/installDocker.sh
-  $DIR/install/installCassandra.sh
+  #$DIR/install/installMaven.sh >> $LOGFILE
+  #$DIR/install/installTomcat8.sh >> $LOGFILE
+  $DIR/install/installNodejs.sh >> $LOGFILE
+  $DIR/install/installDocker.sh >> $LOGFILE
+  $DIR/install/installCassandra.sh >> $LOGFILE
 
-  #$DIR/install/installMongoDb.sh
-  $DIR/install/installChrome.sh
-  $DIR/install/installSublime.sh
-  $DIR/install/installUnitTweak.sh
+  #$DIR/install/installMongoDb.sh >> $LOGFILE
+  $DIR/install/installChrome.sh >> $LOGFILE
+  $DIR/install/installSublime.sh >> $LOGFILE
+  $DIR/install/installUnitTweak.sh >> $LOGFILE
 
   echo "*** ------ install done  ------ ***" 1>&2
 }
