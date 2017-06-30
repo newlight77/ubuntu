1495d85087f1a012d83674ab637280a6fe3202b1
Kong To <newlight77@gmail.com>
2017-06-30 19:07:15 +0200

    fixing docker-ce install script

--- ubuntu/scripts/install/installDocker.sh
+++ ubuntu/scripts/install/installDocker.sh
@@ -9,7 +9,8 @@ installDocker() {
   echo "*** ------ installDocker ------ ***" 1>&2
   $(isRoot)
 
-  add-apt-repository "deb https://apt.dockerproject.org/repo/ ubuntu-$(lsb_release -cs) main"
+  add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  #add-apt-repository "deb https://apt.dockerproject.org/repo/ ubuntu-$(lsb_release -cs) main"
   #add-apt-repository -y "deb https://apt.dockerproject.org/repo ubuntu-xenial main"
   #echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | tee /etc/apt/sources.list.d/docker.list
 
@@ -17,7 +18,7 @@ installDocker() {
   apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
   apt-get install -y apt-transport-https ca-certificates software-properties-common
 
-  curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
   apt-key fingerprint 0EBFCD88
   #apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
